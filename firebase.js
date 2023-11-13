ca7d6fe3eab1212b04d94ec7e75681c84a46ab81
Zach <zachkoh0871@gmail.com>
2023-11-13 21:43:37 +0000

    First commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmCw-vOsERRbmhMNcYFmZ111YgW_lvuj4",
+  authDomain: "todo-b4420.firebaseapp.com",
+  projectId: "todo-b4420",
+  storageBucket: "todo-b4420.appspot.com",
+  messagingSenderId: "229585073127",
+  appId: "1:229585073127:web:1ff8a7e3034cbbb49a8342"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/ToDoList.tsx
@@ -0,0 +1,52 @@
+//import React from "react";
+import React, { useState } from "react";
+
+interface item {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+export const ToDoList: React.FC = () => {
+  const [todos,setTodos] = useState<item[]>([
+    { id: 1, text: "Learn Typescript with George", completed: false },
+    { id: 2, text: "Build Todo List App", completed: false },
+  ]);
+
+  const [input,setInput] = useState<string>("");  
+
+  const handleToggle = (id: number) => {
+      setTodos(
+          todos.map((todo) => {
+              if(todo.id === id) {
+                  return { ...todo, completed: !todo.completed };
+              }
+              return todo;
+          })
+      );
+  };
+
+  const handleClick = () => {
+      const newTodo: item = { id: Date.now(), text: input, completed: false };
+      setTodos([...todos, newTodo]);
+  };
+
+  return (
+    <div className="main-container">
+      <h1>Todo List</h1>
+      <ul>
+          {todos.map((todo) => (
+              <li key={todo.id} onClick={()=> handleToggle(todo.id)} style={{textDecoration: todo.completed ? "line-through" : "none" }}>
+                {todo.text}
+              </li>
+          ))}
+      </ul>
+      <input 
+      type="text" 
+      placeholder="Add todo item" 
+      onChange={(e)=> setInput(e.currentTarget.value)}
+      />
+      <button onClick={handleClick}>Add</button>
+    </div>
+  );  
+};
